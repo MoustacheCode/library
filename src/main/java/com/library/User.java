fa4397a43e2dbe3446127c88d97c86c7eeb23122
Richard <r-duerden@live.co.uk>
2026-05-01 14:39:32 +0100

    basic user class created

--- src/main/java/com/library/User.java
+++ src/main/java/com/library/User.java
@@ -2,5 +2,39 @@ package com.library;
 
 public class User {
 
+    private String username;
+    private String password;
+    private boolean isAdmin;
 
+
+    public User(String userName, String password, boolean isAdmin) {
+        this.username = userName;
+        this.password = password;
+        this.isAdmin = isAdmin;
+
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
 }
